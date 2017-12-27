214aa812c0c9ada2bc22575a89894d5da1db5fa8
KD2565697672 <2565697672@qq.com>
2017-12-27 16:26:44 +0800

    商城控制器

---
+++ application/backsystem/controller/ShopGoods.php
@@ -0,0 +1,18 @@
+<?php 
+namespace app\backsystem\controller;
+
+// use app\backsystem\model\ShopModel;
+
+
+/**
+ * 
+ */
+class ShopGoods extends Base
+{
+	
+	public function index()
+	{
+		echo "111";
+		return $this->fetch();
+	}
+} 
