1929d92dfd172905d98e71a61260ed673338622f
KD2565697672 <2565697672@qq.com>
2017-12-28 10:33:54 +0800

    添加商品后台验证参数

--- application/backsystem/validate/ShopGoodsValidate.php
+++ application/backsystem/validate/ShopGoodsValidate.php
@@ -16,6 +16,7 @@ class ShopGoodsValidate extends Validate
 {
     protected $rule = [
         ['name', 'require', '商品名称不能为空'],
+        ['cid', 'require', '分类不能为空'],
         ['price', 'require', '价格不能为空'],
         ['imgurl', 'require', '商品图片不能为空']
         ['is_under', 'require', '请设置商品是否上架']
